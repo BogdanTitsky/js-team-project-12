7c133218b9e88412624564e2941a39c14254a364
unknown <bogdan.titsky@gmail.com>
2023-06-02 16:32:41 +0300

    main.js import

--- src/js/card-markup.js
+++ src/js/card-markup.js
@@ -1,31 +1,56 @@
 import axios from 'axios';
 
 const filmList = document.querySelector('.catalog-films-list');
-const filmMarkup = createGalleryMarkup(filmList);
+const apiKey = '183c3cacc9c38c09c14d38798ccfe9d7';
 
-const KEY = '183c3cacc9c38c09c14d38798ccfe9d7';
+async function getMovieArray() {
+  const url = `https://api.themoviedb.org/3/trending/movie/week?api_key=${apiKey}&language=en-US`;
+
+  try {
+    const response = await axios.get(url);
+    const data = response.data;
+
+    const movies = data.results;
+
+    return movies;
+  } catch (error) {
+    console.error('Помилка отримання масиву фільмів:', error);
+    return [];
+  }
+}
+
+async function renderMovieList() {
+  try {
+    const movies = await getMovieArray();
+    const markup = makeMarkup(movies);
+    filmList.insertAdjacentHTML('beforeend', markup);
+  } catch (error) {
+    console.error('Помилка при рендерингу списку фільмів:', error);
+  }
+}
 
 function makeMarkup(arr) {
   return arr
     .map(
-      ({ webformatURL, tags, likes, views, comments, downloads }) => `
-      <a href="${webformatURL}" class="photo-card">
-        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-        <div class="info">
-          <p class="info-item">
-            <b>Likes: ${likes}</b>
-          </p>
-          <p class="info-item">
-            <b>Views: ${views}</b>
-          </p>
-          <p class="info-item">
-            <b>Comments: ${comments}</b>
-          </p>
-          <p class="info-item">
-            <b>Downloads: ${downloads}</b>
-          </p>
-        </div>
-      </a>`
+      ({ poster_path, title, vote_average, release_date }) => `
+<li>
+  <a href="" class="photo-card">
+    <img src="https://image.tmdb.org/t/p/w300${poster_path}" alt="${title}" loading="lazy" />
+    <div class="info">
+      <p class="info-item">
+        <b>${title} </b>
+      </p>
+      <p class="info-item">
+        <b>${vote_average}</b>
+      </p>
+      <p class="info-item">
+        <b>${release_date}</b>
+      </p>
+    </div>
+  </a>
+</li>`
     )
     .join('');
 }
+
+renderMovieList();
