8ce66267f595b523d6c07f7d920555a7b896751d
Diana Smilian <dianka.kulakevich@gmail.com>
2023-06-03 19:52:23 +0300

    Replaced "img" with "svg" and added search logic in JavaScript.

--- src/js/catalog.js
+++ src/js/catalog.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { makeMarkup } from './card-markup';
+import Notiflix from 'notiflix';
 const apiKey = '183c3cacc9c38c09c14d38798ccfe9d7';
 
 async function getSearchMovie(query, page=1) {
@@ -22,15 +23,26 @@ async function onSubmitForm(e) {
    e.preventDefault();
   const form = e.currentTarget;
   const value = form.elements.search.value.trim();
+
+  if (value === "") {
+   Notiflix.Notify.failure('No query!');
+return
+}
+
   const getMovie = await getSearchMovie(value);
+
+if (getMovie.length === 0) {
+   return movieList(`<p class='text-error'>OOPS...
+    We are very sorry!
+    We don't have any results matching your search.</p>`)
+   }
+   
   const markupMovie = await makeMarkup(getMovie);
   movieList(markupMovie);
-  console.log(markupMovie)
 }
 
 function movieList(markup) {
    refs.filmList.innerHTML = markup;
 }
-
 
 
