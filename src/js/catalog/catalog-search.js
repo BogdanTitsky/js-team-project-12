18fe1709992f12a64804207a659da1a972afc6d0
Diana Smilian <dianka.kulakevich@gmail.com>
2023-06-03 22:35:17 +0300

    refresh code

--- src/js/catalog/catalog-search.js
+++ src/js/catalog/catalog-search.js
@@ -3,44 +3,55 @@ import { makeMarkup } from '../reuseble/card-markup';
 import Notiflix from 'notiflix';
 const apiKey = '183c3cacc9c38c09c14d38798ccfe9d7';
 
-async function getSearchMovie(query, page = 1) {
-  const response = await axios.get(`
+async function getSearchMovie(query, page=1) {
+   const response = await axios.get(`
 https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}&page=${page}`);
-  const data = await response.data;
-  const movie = await data.results;
-  console.log(movie);
-  return movie;
+const data = await response.data;
+const movie = await data.results;
+console.log(movie)
+return movie;
 }
 
 const refs = {
-  form: document.querySelector('.form-search'),
-  filmList: document.querySelector('.weelky-trends-list'),
-};
+form: document.querySelector('.form-search'),
+filmList: document.querySelector('.weelky-trends-list'),
+textBox: document.querySelector('.catalog-text-box')
+}
 
-refs.form.addEventListener('submit', onSubmitForm);
+refs.form.addEventListener('submit', onSubmitForm)
 
 async function onSubmitForm(e) {
-  e.preventDefault();
+   e.preventDefault();
   const form = e.currentTarget;
   const value = form.elements.search.value.trim();
 
-  if (value === '') {
-    Notiflix.Notify.failure('No query!');
-    return;
-  }
-
+  
+  if (value === "") {
+   Notiflix.Notify.failure('No value!');
+return
+}
+clearMarkup();
   const getMovie = await getSearchMovie(value);
 
-  if (getMovie.length === 0) {
-    return movieList(`<p class='text-error'>OOPS...
+if (getMovie.length === 0) {
+   textError(`<p class='text-error'>OOPS...
     We are very sorry!
-    We don't have any results matching your search.</p>`);
-  }
-
+    We don't have any results matching your search.</p>`)
+    return
+   }
+   
   const markupMovie = await makeMarkup(getMovie);
   movieList(markupMovie);
 }
 
 function movieList(markup) {
-  refs.filmList.innerHTML = markup;
+   refs.filmList.innerHTML = markup;
 }
+
+function textError(markup) {
+   refs.textBox.innerHTML = markup;
+}
+
+function clearMarkup() {
+   refs.filmList.innerHTML = '';
+ }
