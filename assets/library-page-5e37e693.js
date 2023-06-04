ce644aa898a610168aa4d6c874b7d6f5387e2815
BogdanTitsky <119936698+BogdanTitsky@users.noreply.github.com>
2023-06-04 12:30:01 +0000

    Deploying to gh-pages from @ BogdanTitsky/js-team-project-12@baf25064920389a98e18b6747947572e3cb7348c 🚀

--- assets/library-page-5e37e693.js
+++
@@ -1,1 +0,0 @@
-import"./footer-f29026e0.js";const e="message";if(localStorage.length){const o=localStorage.getItem(e);console.log(o)}
---
+++ assets/library-page-a94d4643.js
@@ -0,0 +1,1 @@
+import"./footer-32303f92.js";const e="message";if(localStorage.length){const o=localStorage.getItem(e);console.log(o)}
