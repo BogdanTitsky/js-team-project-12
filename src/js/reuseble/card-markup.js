ccef9142af5d03efa3a116d4f5d3f150da671a1b
unknown <bogdan.titsky@gmail.com>
2023-06-04 08:47:39 +0300

    root added

--- src/js/reuseble/card-markup.js
+++ src/js/reuseble/card-markup.js
@@ -2,18 +2,18 @@ function makeMarkup(arr) {
   return arr
     .map(
       ({ poster_path, title, vote_average, release_date }) => `
-<li>
-  <a href="" class="photo-card">
-    <img src="https://image.tmdb.org/t/p/w300${poster_path}" alt="${title}" loading="lazy" />
+<li class="film-card">
+  <a href="" class="film-card-link">
+    <img class="film-card-img" src="https://image.tmdb.org/t/p/w300${poster_path}" alt="${title}" loading="lazy" />
     <div class="info">
-      <p class="info-item">
-        <b>${title} </b>
+      <p class="info-title">
+        ${title}
       </p>
       <p class="info-item">
-        <b>${vote_average}</b>
+        ${vote_average}
       </p>
       <p class="info-item">
-        <b>${release_date}</b>
+        ${release_date}
       </p>
     </div>
   </a>
