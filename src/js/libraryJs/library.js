bc73556cabf5f7c6adcc21274056b8e5786f165f
Nikita1825 <nikitapostnov1825@gmail.com>
2023-06-05 17:53:57 +0300

    library-Nikita

--- src/js/libraryJs/library.js
+++ src/js/libraryJs/library.js
@@ -3,6 +3,7 @@ const libraryCard = document.querySelector('.library-card');
 const librList = document.querySelector('.libr-list');
 const librCatch = document.querySelector('.libr-catch');
 const key = 'upcomingFilms';
+import { makeStarRating } from '../reuseble/star-rating';
 
 
 if (localStorage.length ) {
@@ -18,14 +19,9 @@ createUpcomingMarkup(parsed);
 
 
 function createUpcomingMarkup(parsed) {
-  const markup = parsed.map(({
-  backdrop_path,
-  poster_path,
-  title,
-  vote_average,
-  release_date,
-}) =>
-   `
+  const markup = parsed.map(
+    ({ poster_path, title, vote_average, release_date }) =>
+      `
   <li class="film-card">
   <a href="" class="film-card-link">
     <img class="film-card-img" src="https://image.tmdb.org/t/p/w300${poster_path}" alt="${title}" loading="lazy" />
@@ -34,15 +30,17 @@ function createUpcomingMarkup(parsed) {
         ${title}
       </p>
       <p class="info-item">
-        ${vote_average}
-      </p>
-      <p class="info-item">
         ${release_date}
       </p>
+      <p class="info-item">
+        ${makeStarRating(vote_average)}
+      </p>
+     
     </div>
   </a>
 </li>`
   )
+  .join('');
   librList.innerHTML = markup;
 }
 
