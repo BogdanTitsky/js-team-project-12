fcaec8a161dd6a8c8e9023346524f166c5685585
ghhjhkjkklkl <99083436+ghhjhkjkklkl@users.noreply.github.com>
2023-06-05 09:17:48 +0200

    style modal

--- src/js/hero/get-trailer.js
+++ src/js/hero/get-trailer.js
@@ -1,20 +1,32 @@
 import axios from 'axios';
 
 const trailerBtn = document.querySelector('.hero-trailer-btn');
+const closeSvg = document.querySelector('.hero-PopUp-closeSvg');
+const modal = document.querySelector('.hero-PopUp');
 
 const apiKey = '183c3cacc9c38c09c14d38798ccfe9d7';
 
-async function fetchTrailers() {
-  const response = await axios.get(`
-  https://api.themoviedb.org/3/movie/100/videos?api_key=${apiKey}&language=en-US`);
+async function getTrailer() {
+  const response = await axios.get(
+    `https://api.themoviedb.org/3/movie/100/videos?api_key=${apiKey}&language=en-US`
+  );
   const data = await response.data;
   const trailer = await data.results;
   console.log(data);
   return trailer;
 }
 
-trailerBtn.addEventListener('click', handleWatchTrailer);
+trailerBtn.addEventListener('click', handlerWatchTrailer);
+closeSvg.addEventListener('click', hideModal());
 
-async function handleWatchTrailer() {
-  fetchTrailers();
+function handlerWatchTrailer() {
+  getTrailer();
+}
+
+// function showModal() {
+//   modal.classList.add('active');
+// }
+
+function hideModal() {
+  modal.classList.add('active');
 }
