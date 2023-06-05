ff1db195b4a0ffefc4f398f4279a834f3ed7bc20
BogdanTitsky <119936698+BogdanTitsky@users.noreply.github.com>
2023-06-05 21:48:26 +0000

    Deploying to gh-pages from @ BogdanTitsky/js-team-project-12@98136734c7c59cd6792d5c5c487c8764b183871c 🚀

--- assets/card-markup-0e99a49d.js
+++ assets/card-markup-0e99a49d.js
@@ -1,4 +1,4 @@
-import{g as t,m as c}from"./footer-143e7997.js";async function p(s){const n=await t();return s.map(({poster_path:a,title:i,genre_ids:e,release_date:r,vote_average:l})=>`
+import{g as t,m as c}from"./footer-b68d9b12.js";async function p(s){const n=await t();return s.map(({poster_path:a,title:i,genre_ids:e,release_date:r,vote_average:l})=>`
 <li class="film-card">
   <a href="" class="film-card-link">
     <img class="film-card-img" src="${a?`https://image.tmdb.org/t/p/w300${a}`:"../../img/comingSoon.jpg"}" alt="${i}" loading="lazy" />
