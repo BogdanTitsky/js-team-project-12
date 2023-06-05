a980d4434be99f52cf97c5f1760c24aba58950b4
BogdanTitsky <119936698+BogdanTitsky@users.noreply.github.com>
2023-06-05 14:01:15 +0000

    Deploying to gh-pages from @ BogdanTitsky/js-team-project-12@22c73f408d60c2988eda4830d35389ecff36d7a5 🚀

--- assets/card-markup-82dc70c4.js
+++ assets/card-markup-82dc70c4.js
@@ -1,4 +1,4 @@
-import{a as c}from"./footer-64011acd.js";function u(e){return`
+import{a as c}from"./footer-55dcca99.js";function u(e){return`
     <div class="rating">
       <div class="rating-body">
         <div class="rating-active"></div>
