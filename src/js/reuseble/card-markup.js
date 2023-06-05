09165e59efb551e6169b5b65a80812d03b4c0568
unknown <bogdan.titsky@gmail.com>
2023-06-05 11:23:09 +0300

    trendsStyles

--- src/js/reuseble/card-markup.js
+++ src/js/reuseble/card-markup.js
@@ -2,29 +2,33 @@ import { makeStarRating } from './star-rating';
 import getGenresMap from './genres';
 
 async function makeMarkup(arr) {
-  const genresMap = await getGenresMap()
+  const genresMap = await getGenresMap();
   return arr
-    .map(
-      ({ poster_path, title, genre_ids, release_date, vote_average }) => {
-        return `
+    .map(({ poster_path, title, genre_ids, release_date, vote_average }) => {
+      return `
 <li class="film-card">
   <a href="" class="film-card-link">
     <img class="film-card-img" src="https://image.tmdb.org/t/p/w300${poster_path}" alt="${title}" loading="lazy" />
     <div class="info">
-      <p class="info-title">
-        ${title}
-      </p>
-      <p class="info-genre">
-        ${genre_ids.map((id) => genresMap[id]).join(', ')} | <span class="info-release-date"> ${release_date}</span>
-      </p>
+<div>
+        <p class="info-title">
+          ${title}
+        </p>
+        <p class="info-genre">
+          ${genre_ids
+            .map(id => genresMap[id])
+            .join(
+              ', '
+            )} | <span class="info-release-date"> ${release_date}</span>
+        </p>
+</div>
       <div class="rating">
         ${makeStarRating(vote_average)}
       </div>
     </div>
   </a>
 </li>`;
-      },
-    )
+    })
     .join('');
 }
 
