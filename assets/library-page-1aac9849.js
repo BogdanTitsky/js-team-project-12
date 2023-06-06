9fda21f1fbd5c1d05dcf48604132a52bc4cc98dc
BogdanTitsky <119936698+BogdanTitsky@users.noreply.github.com>
2023-06-06 14:59:47 +0000

    Deploying to gh-pages from @ BogdanTitsky/js-team-project-12@7b43a60c17d1ea5dff23a7bbb3fd64ecbb53c896 🚀

---
+++ assets/library-page-1aac9849.js
@@ -0,0 +1,39 @@
+import{g as r,m as d}from"./footer-8e53d27a.js";document.querySelector(".libr-list");const m=document.querySelector(".libr-list"),u=document.querySelector(".libr-catch"),p="upcomingFilms";if(localStorage.length){const a=localStorage.getItem(p),s=JSON.parse(a);console.log(s),v(s),s.length>=1&&u.classList.add("display")}async function v(a){const s=await r(),i=a.map(({poster_path:t,title:e,vote_average:c,release_date:l,genre_ids:n})=>`
+  <li class="film-card">
+  <a href="" class="film-card-link">
+    <img class="film-card-img" src="https://image.tmdb.org/t/p/w300${t}" alt="${e}" loading="lazy" />
+     <div class="info">
+        <p class="info-title">
+          ${e}
+        </p>
+      <div class="genre-rating">
+        <p class="info-genre">
+          ${n.slice(0,2).map(o=>s[o]).join(", ")} | <span class="info-release-date"> ${l.substr(0,4)}</span>
+        </p>
+
+        <div class="rating">
+          ${d(c)}
+        </div>
+      </div>
+    </div>
+  </a>
+</li>`).join("");m.innerHTML=i}const $=document.querySelector(".loadmorebtn");$.addEventListener("click",b);function b(){console.log(15);const a=localStorage.getItem(p),s=JSON.parse(a);for(let i=0;i<a.length;i++){const t=r(),e=s.map(({poster_path:c,title:l,vote_average:n,release_date:o,genre_ids:g})=>`
+  <li class="film-card">
+  <a href="" class="film-card-link">
+    <img class="film-card-img" src="https://image.tmdb.org/t/p/w300${c}" alt="${l}" loading="lazy" />
+    <div class="info">
+    <div>
+      <p class="info-title">
+        ${l}
+      </p>
+      <p class="info-genre">
+          ${g.slice(0,2).map(f=>t[f]).join(", ")} | <span class="info-release-date"> ${o.substr(0,4)}</span>
+        </p>
+        </div>
+      <div class="rating">
+        ${d(n)}
+      </div>
+     
+    </div>
+  </a>
+</li>`).join("");m.insertAdjacentHTML=e}}
