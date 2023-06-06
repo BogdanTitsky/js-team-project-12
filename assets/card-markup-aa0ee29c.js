056f1fbefb5d722c79570d6c2839e4d04c76af32
BogdanTitsky <119936698+BogdanTitsky@users.noreply.github.com>
2023-06-06 07:45:18 +0000

    Deploying to gh-pages from @ BogdanTitsky/js-team-project-12@7f75281d4a1706a43e0aa60c451e5e98f3ff3990 🚀

---
+++ assets/card-markup-aa0ee29c.js
@@ -0,0 +1,21 @@
+import{g as t,m as g}from"./footer-b68d9b12.js";async function d(s){const n=await t();return s.map(({poster_path:a,title:i,genre_ids:e,release_date:r,vote_average:l})=>`
+<li class="film-card">
+  <a href="" class="film-card-link">
+    <img class="film-card-img" src="${a?`https://image.tmdb.org/t/p/w300${a}`:"../../img/comingSoon.jpg"}" alt="${i}" loading="lazy" />
+    <div class="darkening"></div>
+    <div class="info">
+        <p class="info-title">
+          ${i}
+        </p>
+      <div class="genre-rating">
+        <p class="info-genre">
+          ${e.slice(0,2).map(c=>n[c]).join(", ")} | <span class="info-release-date"> ${r.substr(0,4)}</span>
+        </p>
+
+        <div class="rating">
+          ${g(l)}
+        </div>
+      </div>
+    </div>
+  </a>
+</li>`).join("")}export{d as m};
