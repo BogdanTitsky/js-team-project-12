9f25e22b3a81b524863dd0f28800828b5461f121
onsnov <114084570+onsnov@users.noreply.github.com>
2023-06-06 17:06:10 +0300

    додає кнопку і ф-цію завантаження з локал сторедж

--- src/js/libraryJs/library.js
+++ src/js/libraryJs/library.js
@@ -20,12 +20,10 @@ if (localStorage.length) {
   } 
 } 
 
-
-
-   
-    
+ 
 
 async function createUpcomingMarkup(parsed) {
+  
     const genresMap = await getGenresMap()
     const markup = parsed
       .map(
@@ -63,3 +61,53 @@ async function createUpcomingMarkup(parsed) {
     librList.innerHTML = markup;
   }
 
+//load more 
+const loadmorebtn = document.querySelector('.loadmorebtn');
+
+ loadmorebtn.addEventListener('click', loadPosts);
+
+function loadPosts() {
+   console.log(15)
+   const posts = localStorage.getItem(key);
+   const parsed = JSON.parse(posts);
+   
+   for (let i = 0; i < posts.length; i++) {
+      const genresMap = getGenresMap()
+    const markup = parsed
+      .map(
+        ({ poster_path, title, vote_average, release_date, genre_ids }) =>
+          `
+  <li class="film-card">
+  <a href="" class="film-card-link">
+    <img class="film-card-img" src="https://image.tmdb.org/t/p/w300${poster_path}" alt="${title}" loading="lazy" />
+    <div class="info">
+    <div>
+      <p class="info-title">
+        ${title}
+      </p>
+      <p class="info-genre">
+          ${genre_ids
+            .slice(0, 2)
+            .map(id => genresMap[id])
+            .join(
+              ', '
+            )} | <span class="info-release-date"> ${release_date.substr(
+            0,
+            4
+          )}</span>
+        </p>
+        </div>
+      <div class="rating">
+        ${makeStarRating(vote_average)}
+      </div>
+     
+    </div>
+  </a>
+</li>`
+      )
+      .join('');
+
+    librList.insertAdjacentHTML =('beforeend',markup) ;
+    }
+   
+ }
