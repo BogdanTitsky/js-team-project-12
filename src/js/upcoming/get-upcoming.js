43ad99f28ff89d8aeaa7a3a97d6d450647f90518
AnnaAFA <anna.fedoruk111@gmail.com>
2023-06-07 10:48:40 +0300

    img

--- src/js/upcoming/get-upcoming.js
+++ src/js/upcoming/get-upcoming.js
@@ -5,7 +5,7 @@ export async function getUpcoming() {
   const upcomingMovies = [];
 
   for (let page = 1; page <= totalPages; page += 1) {
-    const url = `https://api.themoviedb.org/3/movie/upcoming?api_key=183c3cacc9c38c09c14d38798ccfe9d7&page=${page}`;
+    const url = `https://api.themoviedb.org/3/movie/upcoming?api_key=183c3cacc9c38c09c14d38798ccfe9d7&language=en-US&page=${page}`;
 
     try {
       const { data } = await axios.get(url);
