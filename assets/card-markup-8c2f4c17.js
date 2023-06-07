e12f24c8e657163e20f16466114a99d37a11d564
BogdanTitsky <119936698+BogdanTitsky@users.noreply.github.com>
2023-06-07 11:31:02 +0000

    Deploying to gh-pages from @ BogdanTitsky/js-team-project-12@105c1b290a159500a7139f0bb773aeadda9ea1bb 🚀

--- assets/card-markup-8c2f4c17.js
+++ assets/card-markup-8c2f4c17.js
@@ -1,4 +1,4 @@
-import{g as f,m as v}from"./footer-c8cb398d.js";const $="/js-team-project-12/assets/comingSoon-22be8f72.jpg";async function b(e,o=100){const r=await f();return e.map(({poster_path:a,title:s,genre_ids:t,release_date:c,vote_average:l},m)=>{const g=a?`https://image.tmdb.org/t/p/w300${a}`:$,d=m>=o?"hide-mobile":"",n=t.slice(0,2).map(p=>r[p]).join(", "),i=c.substr(0,4);return`
+import{c as f,m as v}from"./footer-49ebefbf.js";const $="/js-team-project-12/assets/comingSoon-22be8f72.jpg";async function b(e,o=100){const c=await f();return e.map(({poster_path:a,title:s,genre_ids:r,release_date:t,vote_average:l},m)=>{const g=a?`https://image.tmdb.org/t/p/w300${a}`:$,d=m>=o?"hide-mobile":"",n=r.slice(0,2).map(p=>c[p]).join(", "),i=t.substr(0,4);return`
 <li class="film-card ${d}">
   <a href="" class="film-card-link">
     <img class="film-card-img" src="${g}" alt="${s}" loading="lazy" />
--- assets/library-page-3f63427d.js
+++ assets/library-page-3f63427d.js
@@ -1,4 +1,4 @@
-import{g as r,m as d}from"./footer-c8cb398d.js";document.querySelector(".libr-list");const m=document.querySelector(".libr-list"),u=document.querySelector(".libr-catch"),p="upcomingFilms";if(localStorage.length){const a=localStorage.getItem(p),s=JSON.parse(a);console.log(s),v(s),s.length>=1&&u.classList.add("display")}async function v(a){const s=await r(),i=a.map(({poster_path:t,title:e,vote_average:c,release_date:l,genre_ids:n})=>`
+import{c as r,m as d}from"./footer-49ebefbf.js";document.querySelector(".libr-list");const m=document.querySelector(".libr-list"),u=document.querySelector(".libr-catch"),p="upcomingFilms";if(localStorage.length){const a=localStorage.getItem(p),s=JSON.parse(a);console.log(s),v(s),s.length>=1&&u.classList.add("display")}async function v(a){const s=await r(),i=a.map(({poster_path:t,title:e,vote_average:c,release_date:l,genre_ids:n})=>`
   <li class="film-card">
   <a href="" class="film-card-link">
     <img class="film-card-img" src="https://image.tmdb.org/t/p/w300${t}" alt="${e}" loading="lazy" />
