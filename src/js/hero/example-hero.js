2c395521c6c193131eaff1d164b7cafd98eef29b
ColonelMelnyk <xiaomiredmia15@gmail.com>
2023-06-07 19:27:41 +0300

    Hero-Preview-Image done

--- src/js/hero/example-hero.js
+++ src/js/hero/example-hero.js
@@ -3,7 +3,7 @@ import { fillRatings, makeStarRating } from '../reuseble/star-rating';
 import openTrailerModal from '../hero/get-trailer';
 
 const keyApi = `183c3cacc9c38c09c14d38798ccfe9d7`;
-const contentBlock = document.querySelector('.hero-content');
+const contentBlock = document.querySelector('.hero');
 
 async function getRandomFilm() {
   try {
@@ -13,34 +13,44 @@ async function getRandomFilm() {
     const informationMovie = info.data;
     return informationMovie.results;
   } catch (error) {
-    console.log('Помилка при рендерингу списку фільмів:', error);
+    console.log('Помилка при запиту списку фільмів:', error);
   }
 }
 
 function loadHeroMarkup(movieArray) {
-  const movie = movieArray[Math.floor(Math.random() * movieArray.length)];
+    const movie = movieArray[Math.floor(Math.random() * movieArray.length)];
   const heroMarkup = `
-  <div>
-           <h1 class="hero-title">${movie.title}</h1>
-          <div class="rating">
+  <div class="container hero-container" style = "background: linear-gradient(86.77deg,#111111 30.38%,rgba(17, 17, 17, 0) 65.61%), url(https://image.tmdb.org/t/p/w1280${movie.backdrop_path});">
+    <div class ="hero-content"> 
+      <h1 class="hero-title">${movie.title}</h1>
+        <div class="rating">
           ${makeStarRating(movie.vote_average)}
-          </div>
-           <p class="hero-text">${movie.overview}</p>
-           <button type="button" class="button watch-trailer" data-id=${movie.id}>Watch trailer</button>
-           <button type="button" class="button-more-details">More details</button>
-           </div>
+        </div>
+          <p class="hero-text">${movie.overview}</p>
+          <button type="button" class="button watch-trailer" data-id=${movie.id}>Watch trailer</button>
+          <button type="button" class="button-more-details">More details</button>
+    </div>
+  <div>
            `;
+ try {
   contentBlock.innerHTML = ``;
   contentBlock.insertAdjacentHTML(`beforeend`, heroMarkup);
   fillRatings(contentBlock);
-  openTrailerModal();
+  openTrailerModal();      
+} catch (error) {
+  console.log('Помилка при рендерингу секції:', error);
+  }
 }
 
 // functions workflow
 
 async function fetchAndRender(){
- const movieArray = await getRandomFilm();
- console.log(movieArray[0]);
+  try {
+  const movieArray = await getRandomFilm();
  loadHeroMarkup(movieArray);
+  } catch (error) {
+    console.log('Помилка при завантаженні секції:', error);
+  }
+ 
 }
 fetchAndRender();
