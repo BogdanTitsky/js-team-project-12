2bc9a9f5be03c55163d6e5f51dbc82d310916b4b
BogdanTitsky <119936698+BogdanTitsky@users.noreply.github.com>
2023-06-07 05:04:33 +0000

    Deploying to gh-pages from @ BogdanTitsky/js-team-project-12@548b7c1f31f01a773dd927a34a2bdd8b2fff41ef 🚀

--- assets/card-markup-6db5ff66.js
+++ assets/card-markup-6db5ff66.js
@@ -1,4 +1,4 @@
-import{g as f,m as v}from"./footer-4efad7bd.js";const $="/js-team-project-12/assets/comingSoon-22be8f72.jpg";async function b(e,o=100){const r=await f();return e.map(({poster_path:a,title:s,genre_ids:t,release_date:c,vote_average:l},m)=>{const g=a?`https://image.tmdb.org/t/p/w300${a}`:$,d=m>=o?"hide-mobile":"",n=t.slice(0,2).map(p=>r[p]).join(", "),i=c.substr(0,4);return`
+import{g as f,m as v}from"./footer-c8cb398d.js";const $="/js-team-project-12/assets/comingSoon-22be8f72.jpg";async function b(e,o=100){const r=await f();return e.map(({poster_path:a,title:s,genre_ids:t,release_date:c,vote_average:l},m)=>{const g=a?`https://image.tmdb.org/t/p/w300${a}`:$,d=m>=o?"hide-mobile":"",n=t.slice(0,2).map(p=>r[p]).join(", "),i=c.substr(0,4);return`
 <li class="film-card ${d}">
   <a href="" class="film-card-link">
     <img class="film-card-img" src="${g}" alt="${s}" loading="lazy" />
