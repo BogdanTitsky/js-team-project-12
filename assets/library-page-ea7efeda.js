3c226f47ded9f8fe7e05d3faedceb7158302834d
BogdanTitsky <119936698+BogdanTitsky@users.noreply.github.com>
2023-06-08 19:07:33 +0000

    Deploying to gh-pages from @ BogdanTitsky/js-team-project-12@be619ba3a0e5723e2a1c80df75fa610d04b50efd 🚀

--- assets/library-page-ea7efeda.js
+++ assets/library-page-ea7efeda.js
@@ -1,4 +1,4 @@
-import{e as r,m as d}from"./footer-1e107698.js";document.querySelector(".libr-list");const g=document.querySelector(".libr-list"),u=document.querySelector(".libr-catch"),m="upcomingFilms";if(localStorage.length){const a=localStorage.getItem(m),s=JSON.parse(a);console.log(s),v(s),s.length>=1&&u.classList.add("display")}async function v(a){const s=await r(),i=a.map(({poster_path:t,title:e,vote_average:n,release_date:l,genre_ids:c})=>`
+import{e as r,m as d}from"./footer-02e8722e.js";document.querySelector(".libr-list");const g=document.querySelector(".libr-list"),u=document.querySelector(".libr-catch"),m="upcomingFilms";if(localStorage.length){const a=localStorage.getItem(m),s=JSON.parse(a);console.log(s),v(s),s.length>=1&&u.classList.add("display")}async function v(a){const s=await r(),i=a.map(({poster_path:t,title:e,vote_average:n,release_date:l,genre_ids:c})=>`
   <li class="film-card">
   <a href="" class="film-card-link">
     <img class="film-card-img" src="https://image.tmdb.org/t/p/w300${t}" alt="${e}" loading="lazy" />
