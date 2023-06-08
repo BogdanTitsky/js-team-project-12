2503fe03cdc5173779eb7609abfd105d3a7ccb40
AnnaAFA <anna.fedoruk111@gmail.com>
2023-06-08 12:17:52 +0300

    add close modal

--- src/js/reuseble/markups.js
+++ src/js/reuseble/markups.js
@@ -11,18 +11,24 @@ function wrapPosterPath(poster_path) {
 export async function makeMarkup(moviesList, mobileCount = 100) {
   const genresMap = await getGenresMap();
   return moviesList
-    .map(({ id, poster_path, title, genre_ids, release_date, vote_average }, i) => {
-      const hideMobile = i >= mobileCount ? 'hide-mobile' : '';
-      const genresString = genre_ids
-        .slice(0, 2)
-        .map(id => genresMap[id])
-        .join(', ');
-      const year = release_date.substr(0, 4)
+    .map(
+      (
+        { id, poster_path, title, genre_ids, release_date, vote_average },
+        i
+      ) => {
+        const hideMobile = i >= mobileCount ? 'hide-mobile' : '';
+        const genresString = genre_ids
+          .slice(0, 2)
+          .map(id => genresMap[id])
+          .join(', ');
+        const year = release_date.substr(0, 4);
 
-      return `
+        return `
 <li class="film-card ${hideMobile}">
   <a href="#" class="film-card-link" movie_id="${id}">
-    <img class="film-card-img" src="${wrapPosterPath(poster_path)}" alt="${title}" loading="lazy" />
+    <img class="film-card-img" src="${wrapPosterPath(
+      poster_path
+    )}" alt="${title}" loading="lazy" />
     <div class="darkening"></div>
     <div class="info">
         <p class="info-title">
@@ -30,8 +36,10 @@ export async function makeMarkup(moviesList, mobileCount = 100) {
         </p>
       <div class="genre-rating">
         <p class="info-genre">
-          ${genresString !== "" ? genresString : "Unknown"} |
-          <span class="info-release-date"> ${year !== "" ? year : "Unknown"}</span>
+          ${genresString !== '' ? genresString : 'Unknown'} |
+          <span class="info-release-date"> ${
+            year !== '' ? year : 'Unknown'
+          }</span>
         </p>
 
         <div class="rating">
@@ -41,18 +49,29 @@ export async function makeMarkup(moviesList, mobileCount = 100) {
     </div>
   </a>
 </li>`;
-    })
+      }
+    )
     .join('');
 }
 
-export function createModalMarkup({ title, poster_path, vote_average, vote_count, popularity, genres, overview }) {
-  return `<div class="modal">
+export function createModalMarkup({
+  title,
+  poster_path,
+  vote_average,
+  vote_count,
+  popularity,
+  genres,
+  overview,
+}) {
+  return `<div class="modal-wrap">
       <button class="modal__close" type="button">
         <svg class="modal__svg" width="24" height="24">
           <use href=""></use>
         </svg>
       </button>
-      <img src="${wrapPosterPath(poster_path)}" alt="${title}" class="modal__img" />
+      <img src="${wrapPosterPath(
+        poster_path
+      )}" alt="${title}" class="modal__img" />
       <div class="modal__wrap">
         <h2 class="modal__title">${title}</h2>
         <ul class="modal-list">
@@ -69,7 +88,9 @@ export function createModalMarkup({ title, poster_path, vote_average, vote_count
           </li>
           <li class="modal-list__item">
             <span class="modal-list__text">Genre</span>
-            <span class="modal-list__genres">${genres.map(({ name }) => name).join(', ')}</span>
+            <span class="modal-list__genres">${genres
+              .map(({ name }) => name)
+              .join(', ')}</span>
           </li>
         </ul>
         <p class="modal__about">About</p>
